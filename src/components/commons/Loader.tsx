a02b882120e8a5ea3d9500bf7fa93474b4d07104
Tabi Idris <tabidrizy@gmail.com>
2025-10-15 10:00:21 +0100

    fixing format errors

--- src/components/commons/Loader.tsx
+++ src/components/commons/Loader.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Loader: React.FC = () => {
   return (
-    <div className="absolute inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
+    <div className="absolute inset-0 flex items-center justify-center bg-gray bg-opacity-50">
       <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
+      <span className="sr-only text-gray-700">Loading...</span>
     </div>
   );
 };
